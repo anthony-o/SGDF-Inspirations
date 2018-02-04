9c11dd7d5983721d985b2ebf81b7497e909b1c03
Anthony O <netangel+github@gmail.com>
2018-02-04 21:47:59 +0100

    Réorganisation des ateliers dans un seul fichier zip afin de permettre de lister les fichiers grâce à l'API de JSZip

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,6 +11,7 @@ import {Observer} from "rxjs/Observer";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
 import marked from "marked";
+import * as JSZip from "jszip";
 
 @Injectable()
 export class DataService {
@@ -32,16 +33,26 @@ export class DataService {
     this.themesObservable = Observable.create(observer => {
       this.themesObserver = observer;
     });
-    this.themesObservable.subscribe(themes => {console.log(themes)});
+    this.themesObservable.subscribe(themes => {});
 
     this.ateliersObservable = Observable.create(observer => {
       this.ateliersObserver = observer;
     });
-    this.ateliersObservable.subscribe(ateliers => {console.log(ateliers)});
+    this.ateliersObservable.subscribe(ateliers => {});
 
     setTimeout(() => {
       this.init(); // initialisation dans un timeout sinon les variables observer n'ont pas encore été initialisées
     }, 20);
+  }
+
+  private handleError(error) {
+    let errorMessage = "Erreur lors de l'obtention des données : " + error.message;
+    this.alertCtrl.create({
+      title: "Erreur lors de l'obtention des données",
+      subTitle: errorMessage,
+      buttons: ['Ignorer']
+    });
+    console.error(errorMessage);
   }
 
   init() {
@@ -50,72 +61,65 @@ export class DataService {
     this.trancheAges = new Map();
     this.ateliers = [];
 
-    this.http.get('/assets/ateliers/index.txt', {responseType: 'text'})
-      .subscribe(index => {
-        for (let atelierUrl of index.split(/\r?\n/)) {
-          if (atelierUrl) {
-            this.http.get(atelierUrl, {responseType: 'text'})
-              .catch(error => { //TODO à tester, pas sûr que ce bloc affiche correctement le message d'erreur à l'utilisateur
-                let errorMessage = "Erreur lors de l'obtention des données : " + error.message;
-                this.alertCtrl.create({
-                  title: "Erreur lors de l'obtention des données",
-                  subTitle: errorMessage,
-                  buttons: ['Ignorer']
-                });
-                console.error(errorMessage);
-                return [];
-              })
-              .subscribe(atelierMd => {
-                // Découpage du fichier source d'atelier en parties
-                let atelierParts:any = {};
-                for (let part of atelierMd.split(/^# -->/m)) {
-                  if (part) {
-                    let partElements = /^(.+)$\s+([\s\S]*)/m.exec(part); // Utilisation de [\s\S] au lieu de . pour matcher les retours chariots https://stackoverflow.com/a/16119722/535203
-                    atelierParts[partElements[1]
-                      .toLowerCase() // en minuscule
-                      .normalize('NFD').replace(/[\u0300-\u036f]/g, "") // suppression des accents grâce à https://stackoverflow.com/a/37511463/535203
-                      .replace(/\W/g, '') // suppression des charactères autres qu'alphanumériques
-                      ] = partElements[2].trim();
-                  }
-                }
-                // Création de l'objet Atelier avec ces parties
-                let themeStr = atelierParts.theme;
-                let theme: Theme = this.themes.get(themeStr);
-                if (!theme) {
-                  theme = new Theme(themeStr);
-                  this.themes.set(themeStr, theme);
-                  this.themesObserver.next(this.getThemesValues()); // Rafraîchissement des thèmes
-                }
-
-                let atelier: Atelier = {
-                  sousTheme: atelierParts.soustheme,
-                  accueil: marked(atelierParts.accueil),
-                  parole: {
-                    titre: atelierParts.paroletitre,
-                    texte: marked(atelierParts.paroletexte),
-                    reference: atelierParts.parolereference
-                  },
-                  geste: new Geste(
-                    atelierParts.gestetitre,
-                    marked(atelierParts.gestetexte),
-                  ),
-                  envoi: marked(atelierParts.envoi),
-                  trancheAges: atelierParts.tranchesdages.split(',')
-                    .map(label => {
-                      let trancheAge = this.trancheAges.get(label) || new TrancheAge(label);
-                      this.trancheAges.set(label, trancheAge);
-                      return trancheAge;
-                    })
-                };
-
-                this.ateliers.push(atelier);
-                theme.ateliers.push(atelier);
-
-                this.ateliersObserver.next(this.ateliers); // Rafraîchissement des ateliers
-              });
-          }
-        }
+    this.http.get('/assets/data.zip', {responseType: 'arraybuffer'})
+      .subscribe(dataZip => {
+        new JSZip().loadAsync(dataZip).then(jsZip => {
+          jsZip.folder("ateliers").forEach((relativePath, file) => {
+            file.async('text').then(atelierMd => {
+              this.parseAtelierMd(atelierMd);
+            }, this.handleError);
+          });
+        }, this.handleError);
       });
+  }
+
+  private parseAtelierMd(atelierMd): void {
+    // Découpage du fichier source d'atelier en parties
+    let atelierParts: any = {};
+    for (let part of atelierMd.split(/^# -->/m)) {
+      if (part) {
+        let partElements = /^(.+)$\s+([\s\S]*)/m.exec(part); // Utilisation de [\s\S] au lieu de . pour matcher les retours chariots https://stackoverflow.com/a/16119722/535203
+        atelierParts[partElements[1]
+          .toLowerCase() // en minuscule
+          .normalize('NFD').replace(/[\u0300-\u036f]/g, "") // suppression des accents grâce à https://stackoverflow.com/a/37511463/535203
+          .replace(/\W/g, '') // suppression des charactères autres qu'alphanumériques
+          ] = partElements[2].trim();
+      }
+    }
+    // Création de l'objet Atelier avec ces parties
+    let themeStr = atelierParts.theme;
+    let theme: Theme = this.themes.get(themeStr);
+    if (!theme) {
+      theme = new Theme(themeStr);
+      this.themes.set(themeStr, theme);
+      this.themesObserver.next(this.getThemesValues()); // Rafraîchissement des thèmes
+    }
+
+    let atelier: Atelier = {
+      sousTheme: atelierParts.soustheme,
+      accueil: marked(atelierParts.accueil),
+      parole: {
+        titre: atelierParts.paroletitre,
+        texte: marked(atelierParts.paroletexte),
+        reference: atelierParts.parolereference
+      },
+      geste: new Geste(
+        atelierParts.gestetitre,
+        marked(atelierParts.gestetexte),
+      ),
+      envoi: marked(atelierParts.envoi),
+      trancheAges: atelierParts.tranchesdages.split(',')
+        .map(label => {
+          let trancheAge = this.trancheAges.get(label) || new TrancheAge(label);
+          this.trancheAges.set(label, trancheAge);
+          return trancheAge;
+        })
+    };
+
+    this.ateliers.push(atelier);
+    theme.ateliers.push(atelier);
+
+    this.ateliersObserver.next(this.ateliers); // Rafraîchissement des ateliers
   }
 
   private getThemesValues(): Theme[] {
