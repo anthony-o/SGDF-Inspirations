fa4c56004f0525aa48dba9c9cfdae33a20761be5
Anthony O <netangel+github@gmail.com>
2018-06-21 23:39:40 +0200

    Passage à nouveau avec un mode d'intégration des documents simples via un simple parsing HTML et non un rendering de template Angular (donc pas de possibilité d'utiliser `<ion-icon>`)

--- src/pages/simple-document/simple-document.ts
+++ src/pages/simple-document/simple-document.ts
@@ -1,44 +1,47 @@
-import { Component, ComponentFactory, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { OpenHomePage } from '../open-home';
 import { DataService } from '../../app/data.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'page-simple-document',
   templateUrl: 'simple-document.html',
 })
-export class SimpleDocumentPage extends OpenHomePage implements OnInit {
+export class SimpleDocumentPage extends OpenHomePage {
   simpleDocumentFileName: string;
-  // content$: Observable<SafeHtml>;
+  content$: Observable<SafeHtml>;
   navBarTitle: string;
 
-  @ViewChild('container', { read: ViewContainerRef }) container: ViewContainerRef;
+  // @ViewChild('container', { read: ViewContainerRef }) container: ViewContainerRef;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public dataService: DataService,
-    // private sanitizer: DomSanitizer,
+    private sanitizer: DomSanitizer,
   ) {
     super(navCtrl);
     this.simpleDocumentFileName = this.navParams.get('simpleDocumentFileName');
-    // this.content$ = dataService.getSimpleDocumentByFileName(simpleDocumentFileName)
-    //   .pipe(map((content) => this.sanitizer.bypassSecurityTrustHtml(content)));
+    this.content$ = dataService.getSimpleDocumentByFileName(this.simpleDocumentFileName)
+      .pipe(map((content) => this.sanitizer.bypassSecurityTrustHtml(content)));
     this.navBarTitle = this.simpleDocumentFileName;
   }
 
-  ngOnInit() {
-    this.dataService.getSimpleDocumentComponentFactoryByFileName(this.simpleDocumentFileName).subscribe((componentFactory) => {
-      this.addComponent(componentFactory);
-    });
-  }
-
-  // Création dynamique de template grâce à https://stackoverflow.com/a/39507831/535203
-  private addComponent(componentFactory: ComponentFactory<any>, properties: any = {}) {
-    const component = this.container.createComponent(componentFactory);
-    Object.assign(component.instance, properties);
-    // If properties are changed at a later stage, the change detection
-    // may need to be triggered manually:
-    // component.changeDetectorRef.detectChanges();
-  }
+  // ngOnInit() {
+  //   this.dataService.getSimpleDocumentComponentFactoryByFileName(this.simpleDocumentFileName).subscribe((componentFactory) => {
+  //     this.addComponent(componentFactory);
+  //   });
+  // }
+  //
+  // // Création dynamique de template grâce à https://stackoverflow.com/a/39507831/535203
+  // private addComponent(componentFactory: ComponentFactory<any>, properties: any = {}) {
+  //   const component = this.container.createComponent(componentFactory);
+  //   Object.assign(component.instance, properties);
+  //   // If properties are changed at a later stage, the change detection
+  //   // may need to be triggered manually:
+  //   // component.changeDetectorRef.detectChanges();
+  // }
 }
